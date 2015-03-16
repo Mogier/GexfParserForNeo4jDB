12daba7ccbfb1a9b74b05ad20512516a6b71c952
mael <mael.ogier.38@gmail.com>
2015-03-16 17:52:42 +0100

    Parser ok

--- src/main/GexfParser.java
+++ src/main/GexfParser.java
@@ -1,6 +1,69 @@
 package main;
 
-public class GexfParser {
+import java.io.File;
 
+import org.gephi.data.attributes.api.AttributeController;
+import org.gephi.data.attributes.api.AttributeModel;
+import org.gephi.filters.api.FilterController;
+import org.gephi.graph.api.DirectedGraph;
+import org.gephi.graph.api.Graph;
+import org.gephi.graph.api.GraphController;
+import org.gephi.graph.api.GraphModel;
+import org.gephi.io.importer.api.Container;
+import org.gephi.io.importer.api.EdgeDefault;
+import org.gephi.io.importer.api.ImportController;
+import org.gephi.io.processor.plugin.DefaultProcessor;
+import org.gephi.preview.api.PreviewController;
+import org.gephi.preview.api.PreviewModel;
+import org.gephi.project.api.ProjectController;
+import org.gephi.project.api.Workspace;
+import org.gephi.ranking.api.RankingController;
+import org.openide.util.Lookup;
+
+
+public class GexfParser {
+	
+	protected String filePath;	
+
+	public GexfParser(String filePath) {
+		super();
+		this.filePath = filePath;
+	}
+
+	public Graph getGraph() {
+		//Init a project - and therefore a workspace
+		ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
+		pc.newProject();
+		Workspace workspace = pc.getCurrentWorkspace();
+
+		//Get models and controllers for this new workspace - will be useful later
+		AttributeModel attributeModel = Lookup.getDefault().lookup(AttributeController.class).getModel();
+		GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getModel();
+		PreviewModel model = Lookup.getDefault().lookup(PreviewController.class).getModel();
+		ImportController importController = Lookup.getDefault().lookup(ImportController.class);
+		FilterController filterController = Lookup.getDefault().lookup(FilterController.class);
+		RankingController rankingController = Lookup.getDefault().lookup(RankingController.class);
+
+		//Import file       
+		Container container;
+		try {
+		    File file = new File(filePath);
+		    container = importController.importFile(file);
+		    container.getLoader().setEdgeDefault(EdgeDefault.DIRECTED);   //Force DIRECTED
+		} catch (Exception ex) {
+		    ex.printStackTrace();
+		    return null;
+		}
+		System.out.println("File open : " + filePath);
+		//Append imported data to GraphAPI
+		importController.process(container, new DefaultProcessor(), workspace);
+
+		//See if graph is well imported
+		DirectedGraph graph = graphModel.getDirectedGraph();
+		System.out.println("Nodes: " + graph.getNodeCount());
+		System.out.println("Edges: " + graph.getEdgeCount());
+		
+		return graph;
+	}
 	
 }
