a60c8718762512ea9f5f644b9d4c1bec85b8ab77
mael <mael.ogier.38@gmail.com>
2015-03-16 17:52:42 +0100

    Parser ok

--- src/main/GexfParserForNeo4jDB.java
+++ src/main/GexfParserForNeo4jDB.java
@@ -1,13 +1,20 @@
 package main;
 
+import org.gephi.graph.api.Graph;
+
 public class GexfParserForNeo4jDB {
 
+	static final String PATH_TO_FILE = "/home/mael/git/terms-analysis/generatedFiles/im2.gexf";
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		GexfParser geParser = new GexfParser(PATH_TO_FILE);
+		
+		Graph graph = geParser.getGraph();
+		
 	}
 
 }
